d1d0cca660f42ca2097da8684ecf3f0563ced5d4
Orange <280790081@qq.com>
2022-02-26 14:45:28 +0800

    更新

--- dynamic-provider/src/main/java/io/sofastack/dynamic/provider/IndexController.java
+++ dynamic-provider/src/main/java/io/sofastack/dynamic/provider/IndexController.java
@@ -1,34 +1,34 @@
-//package io.sofastack.dynamic.provider;
-//
-//import io.sofastack.dynamic.facade.StrategyService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// * index controller
-// *
-// * @author caojie.cj@antfin.com
-// * @since 2020/2/11
-// */
-//@RestController
-//public class IndexController {
-//
-//    @Autowired
-//    private DemoRepository demoRepository;
-//
-//    @Autowired
-//    private StrategyService strategyService;
-//
-//    @RequestMapping("/dynamicProvider")
-//    public String index() {
-//        return strategyService.message();
-//    }
-//
-//    @RequestMapping("/count")
-//    public Long count() {
-//        return demoRepository.count();
-//    }
-//
-//
-//}
+package io.sofastack.dynamic.provider;
+
+import io.sofastack.dynamic.facade.StrategyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * index controller
+ *
+ * @author caojie.cj@antfin.com
+ * @since 2020/2/11
+ */
+@RestController
+public class IndexController {
+
+    @Autowired
+    private DemoRepository demoRepository;
+
+    @Autowired
+    private StrategyService strategyService;
+
+    @RequestMapping("/dynamicProvider")
+    public String index() {
+        return strategyService.message();
+    }
+
+    @RequestMapping("/count")
+    public Long count() {
+        return demoRepository.count();
+    }
+
+
+}
